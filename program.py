bf2c706b311f3b09b40aa394bac17bb125c267cf
Tommy <englerttw@mymail.vcu.edu>
2018-01-16 09:49:45 -0500

    updated actors.py and program.py
    Added the following:
    ~ specialized (derived) classes
    ~ creature (class) hierarchy
    ~ inheritance
    ~ polymorphism

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ import random
 
 import time
 
-from actors import Wizard, Creature
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 def main():
     #print welcome screen
@@ -20,11 +20,11 @@ def print_header():
 def game_loop():
 
     creatures = [
-        Creature('Toad', 1),
+        SmallAnimal('Toad', 1),
         Creature('Tiger', 12),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000),
+        SmallAnimal('Bat', 3),
+        Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 1000),
     ]
 
     hero = Wizard('Gandalf', 75)
@@ -42,7 +42,7 @@ def game_loop():
 
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
         if cmd == 'a':
-            if hero.attack(active_creature,):
+            if hero.attack(active_creature):
                 creatures.remove(active_creature)
             else:
                 print("The wizard runs and hides, taking time to recover...")
@@ -58,6 +58,10 @@ def game_loop():
         else:
             print('Ok, exiting game ... bye!')
             break
+
+        if not creatures:
+            print("You've defeated all the creatures, well done!!")
+
 
         print()
 
